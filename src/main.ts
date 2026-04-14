571dd46272f3c897c500ce137039e52726bf1311
kartick <kartick.sadhu@weloin.com>
2026-04-14 21:49:39 +0530

    feat: bootstrap with cookie-parser, global /api prefix, CORS, filter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  app.use(cookieParser());
+  app.setGlobalPrefix('api');
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN?.split(',') ?? true,
+    credentials: true,
+  });
+
+  const port = process.env.PORT ?? 3001;
+  await app.listen(port);
+  new Logger('Bootstrap').log(`API listening on :${port}`);
 }
-bootstrap();
+void bootstrap();
